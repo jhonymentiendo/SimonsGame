bb3a12e8fcc7e0f62f8c5824564dd2dcafcb7b0a
Juan Carlos Iglesias Ruiz <jhonymentiendo@gmail.com>
2023-05-11 18:42:14 -0600

    20230511

--- game2.js
+++ game2.js
@@ -7,6 +7,14 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
+$(document).dblclick(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
